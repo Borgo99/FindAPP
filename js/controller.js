f9f0f7be6d89c44c537eb2eb2019394b89a51b8a
Borgo99 <andrea123borgo@gmail.com>
2020-09-22 17:54:36 +0200

    up

--- js/controller.js
+++ js/controller.js
@@ -4,9 +4,12 @@
         body: document.querySelector("body"),
         window1: document.querySelector(".window1"),
         window2: document.querySelector(".window2"),
+        window3: document.querySelector(".window3"),
         box_cat_left: document.querySelector(".box-categories-left"),
         hand_icon: document.querySelector(".hand_icon"),
-        btn1: document.querySelector("#start-button")
+        btn1: document.querySelector("#start-button"),
+        btn2: document.querySelector("#search-button"),
+        module_active: document.querySelector("#module-active")
     }
 
     //main page button click event
@@ -28,10 +31,48 @@
 
     })
 
+    //search button event
+    DOMs.btn2.addEventListener("click", function(){
+
+        setTimeout(function() {
+
+            DOMs.window2.style.opacity = "0";
+            setTimeout(function() {
+                DOMs.window2.style.display = "none";
+                DOMs.window3.style.display = "flex";
+                setTimeout(function() {
+                    DOMs.window3.style.opacity = "1";
+                }, 450);
+            }, 900);
+
+        }, 800);
+
+    })
+
     //manage of hand icon display
     DOMs.box_cat_left.addEventListener("scroll", function() {
         DOMs.hand_icon.style.display = "none";
     });
+
+    //manage module touch event
+    var module_functions = {
+        simpleTouch: function() {
+            DOMs.module_active.style.transform = "scale(.9)";
+        },
+
+        touchLeft: function() {
+            DOMs.module_active.style.transform = "scale(1)";
+        },
+
+        drag: function() {
+            DOMs.module_active.removeEventListener("touchend", module_functions.touchLeft, false);
+            DOMs.module_active.style.transform = "scale(.9) translateX(-120vw)";
+        }
+    }
+    DOMs.module_active.addEventListener("touchstart", module_functions.simpleTouch, false);
+    DOMs.module_active.addEventListener("touchend", module_functions.touchLeft, false);
+    DOMs.module_active.addEventListener("touchmove", module_functions.drag, false);
+
 
 
     /*
